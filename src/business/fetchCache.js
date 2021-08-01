5c067fad1de277af03ffbd2eabd2f47e9719fc72
anas <anas.saber@handicap24.com>
2021-08-02 00:42:43 +0200

    reduce download

--- src/business/fetchCache.js
+++ src/business/fetchCache.js
@@ -1,4 +1,11 @@
 const fetch = require("node-fetch");
+
+const files = {
+    "https://siasky.net/AABR0XVHg7SDyVKNk0xqHnkojFIzharCC3QtpyTLHYojDg": "withdraw_proving_key.bin",
+    "https://siasky.net/CADe3pSNzXpMp4ZaNeyZMefnKv1fmRIsfuyP8EYOWpPLAA": "withdraw_verification_key.json",
+    "https://siasky.net/AABPstSjFBuv3WUNXRnk4tc9NZ1rlt8S04ta8WzbqPH_NA": "withdraw.json",
+};
+
 const cache = {};
 
 module.exports = async (url) => {
@@ -8,6 +15,13 @@ module.exports = async (url) => {
         json: async () => {
             if (content) {
                 return content;
+            } else if (files[url] && files[url].endsWith('.json')) {
+                try {
+                    cache[url] = require('../' + files[url]);
+                    return cache[url];
+                } catch (e) {
+                    console.log(`file not found ../${files[url]}`);
+                }
             }
             const response = await fetch(url);
             const json = await response.json();
